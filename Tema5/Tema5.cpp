8be75e8d6a7a84e8d6f2e09f9aabd87f2cf219f7
Ana <anaopris68@gmail.com>
2022-07-12 23:07:39 +0300

    Add project files.

---
+++ Tema5/Tema5.cpp
@@ -0,0 +1,131 @@
+// Tema5.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	//Cerinta Ex 1:
+	//
+	//Scrieti un program care citind de la tastaturra cele trei dimensiuni ale unui paralelipiped dreptunghic, va afisa volumul lui.
+	//Volumul paralelipipedului dreptunghic : V= L * l * h , unde L= lungime, l= latime, h= inaltime.
+	//
+	//Date de intrare :Programul va citi de la tastatura valorile  Lungimii, latimii si a inaltimii.
+	//
+	//Date de iesire : Rezultatul volumului.
+	
+
+	/*int Lungime, latime, inaltime;
+
+	cout << "Introduceti de la tastatura cele trei dimensiuni ale unui paralelipiped : " << endl;
+
+	cin >> Lungime >> latime >> inaltime;
+
+	int Volum = Lungime * latime * inaltime;
+
+	cout << "Afisam rezultatul volumului: " <<Volum ;*/
+
+
+
+	//Cerinta Ex2 :
+	//
+	//Scrieti un program care va calcula media aritmetica a trei numere citite de la tastatura.
+	//
+	//Date de intrare: Programul va citi de la tastatura cele trei numere : numar1, numar2, numar3.
+	//
+	//
+	//Date de iesire: rezultatul mediei aritmetice.
+
+
+
+	/*int numar1, numar2, numar3;
+
+	cout << "Introduceti trei numere de la tastatura :" << endl;
+
+	cin >> numar1 >> numar2 >> numar3;
+
+	int medieAritmetica = (numar1 + numar2 + numar3) / 3;
+
+	cout << "Rezultatul mediei aritmetice este : " << medieAritmetica;*/
+
+
+
+	//Cerinta Ex3 :
+	//
+	//Scrieti un program care va afisa ultima cifra a unui numar intreg citit de la tastatura.
+	//
+	//Date de intrare : Programul va citi de la tastatura un numar intreg.
+	//
+	//Date de iesire : Progamul va afisa ultima cifra a numarului intreg.
+	
+	
+	
+	/*int numarIntreg;
+
+	cout << "Introduceti numarul de la tastatura :" << endl;
+
+	cin >> numarIntreg;
+
+	int ultimaCifra = numarIntreg % 10;
+
+	cout << "Ultima cifra a numarului introdus de la tastatura este : " << ultimaCifra;*/
+
+
+
+	//Cerinta Ex4 :
+	//
+	//Scrieti un program care va afisa semnul unui numar citit de la tastatura
+	//
+	//Daca numarul este pozitiv, va afisa “+”
+	//Daca numarul este negativ, va afisa “-”
+	//Daca numarul este 0, va afisa “0”
+
+	
+	/*int numar;
+
+	cout << "Introduceti numarul de la tastatura : " << endl;
+
+	cin >> numar;
+
+	if (numar > 0)
+
+	{
+		cout << " + ";
+	}
+
+	else if (numar < 0)
+
+	{
+		cout << " - ";
+	}
+	
+	if (numar == 0)
+
+	{
+		cout << " 0 ";
+	}*/
+
+
+	//Cerinta Ex5 :
+	//
+	//Se citesc doua numere de la tastatura, x,y. Scrieti un program care va afisa cele doua valori in ordine crescatoare.
+	//
+	//Exemplu: citim ,9,0 Afisam : 0 9
+
+
+	/*int numarX, numary;
+
+	cout << "Introduceti de la tastatura cele doua numere : " << endl;
+
+	cin >> numarX >> numarY; */
+
+
+
+
+
+
+
+
+}
